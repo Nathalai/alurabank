51c0348a009476ef8b2774bd72f72c706722ed47
Nathalia Luiza Schedler Calza <nlscalza@gmail.com>
2022-05-03 16:08:01 -0300

    creates ts files; sets tsconfig.json

--- app/app.ts
+++ app/app.ts
@@ -1,4 +1,5 @@
 import { Negociacao } from "./models/negociacao.js";
+
 const negociacao1 = new Negociacao(new Date(), 10, 100);
 console.log(negociacao1);
 console.log(negociacao1.volume);
--- app/models/negociacao.ts
+++ app/models/negociacao.ts
@@ -1,25 +1,32 @@
 export class Negociacao {
+    private _data;
+    private _quantidade;
+    private _valor;
+
     constructor(data, quantidade, valor) {
         if (data instanceof Date) {
             this._data = data;
-        }
-        else {
-            console.log("Imput de data inválido");
+        } else {
+            console.log("Imput de data inválido")
             return;
-        }
+        }        
         this._quantidade = quantidade;
         this._valor = valor;
     }
-    get data() {
+
+    get data(){
         return this._data;
     }
-    get quantidade() {
+
+    get quantidade(){
         return this._quantidade;
     }
-    get valor() {
+
+    get valor(){
         return this._valor;
     }
-    get volume() {
+
+    get volume(){
         const volume = this._quantidade * this._valor;
         return volume;
     }
