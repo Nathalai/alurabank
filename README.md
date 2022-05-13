dd4ee588844221449a867996bbafdf2f654170b6
Nathalia Luiza Schedler Calza <nlscalza@gmail.com>
2022-05-13 14:31:36 -0300

    adds performance decorator; updates readme

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # alurabank
+Projeto de uma aplicação de criação e exibição de negociações, desenvolvida durante a formação em Typescript da Alura.
+
+Para rodar a aplicação na sua máquina, siga estes passos:
+1. Clone o repositório para o seu computador;
+2. Abra o terminal na pasta do projeto;
+3. Rode o comando "npm i" para instalar as dependências do projeto;
+4. Rode o comando "npm run start" para iniciar a aplicação (ela deverá abrir em uma nova aba do seu browser de internet).
--- app/src/views/view.ts
+++ app/src/views/view.ts
@@ -1,3 +1,5 @@
+import { logarTempoDeExecucao } from "../decorators/logar-tempo-de-execucao.js";
+
 export abstract class View<T> {
 
     protected elemento: HTMLElement;
@@ -17,7 +19,8 @@ export abstract class View<T> {
 
     protected abstract template(model: T): string;
 
-    public update(model: T): void {
+    @logarTempoDeExecucao()
+    public update(model: T): void {        
         let template = this.template(model);
         if (this.escapar) {
             template = template.replace(/<script>[\s\S]*?<script>/, '');
